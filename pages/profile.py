c5257d6befbe094a66f1db8a0e61b859f3881daf
Fabrini Copetti <fabrinicopetti@gmail.com>
2025-04-04 18:18:26 -0300

    acrescenta pagina profile no app

---
+++ pages/profile.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.set_page_config(page_title="Profile", page_icon="👤")
+
+st.title("Meu Currículo 👤")
+st.write("---")
+
+# Seção 1: Dados Pessoais
+st.header("Informações Pessoais")
+st.write("**Nome:** Fabrini Copetti")  
+st.write("**Email:** [link](fabrini.copetti@unesp.br)fabrinicopetti@gmail.com / fabrinicopetti@outlook.com")
+
+# Seção 2: Formação Acadêmica
+st.header("Formação")
+st.write("**Cursando Doutorado em Biodiversidade em Ambientes Costeiros** - Universidade Estadual Paulista (2023-   )")
+st.write("**Graduação em Ciências do Mar** - Universidade Federal de São Paulo (2012-2014)")
+
+# Seção 3: Habilidades
+st.header("Habilidades")
+st.markdown("""
+- Visão Computacional (OpenCV, Python)  
+- Aquicultura Sustentável  
+- Análise de Dados  
+""")
+
+# Link de volta para a página principal
+st.markdown("[Voltar para a análise →](/)")
